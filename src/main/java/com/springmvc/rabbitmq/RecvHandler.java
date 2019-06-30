2101ae8d4ecc3ee6b547891af3e7140462a0f878
wolf88844 <wolf88840@163.com>
2019-06-30 10:24:49 +0800

    confirm模式

--- src/main/java/com/springmvc/rabbitmq/RecvHandler.java
+++ src/main/java/com/springmvc/rabbitmq/RecvHandler.java
@@ -2,12 +2,14 @@ package com.springmvc.rabbitmq;
 
 import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.stereotype.Service;
 
 
 
 @Service("recvHandler")
+@RabbitListener(containerFactory = "simpleMessageListenerContainer")
 public class RecvHandler implements ChannelAwareMessageListener {
 
 
@@ -15,8 +17,8 @@ public class RecvHandler implements ChannelAwareMessageListener {
     public void onMessage(Message message, Channel channel) throws Exception {
         try{
             System.out.println("consumer--:"+message.getMessageProperties()+":"+new String(message.getBody()));
-            //channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
-            channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,false);
+            channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+            //channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,true);
         }catch (Exception e){
             e.printStackTrace();
             channel.basicNack(message.getMessageProperties().getDeliveryTag(),false,false);
